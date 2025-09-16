9d7654e6a0ca4cff9fcba7554507d8197aa31bc8
Sho Hashimoto <hashimoto@shokai.org>
2025-09-16 23:02:48 +0900

    standard廃止、prettierを導入してgithub actionsでチェックする (#3)
    * uninstall standard, install prettier
    
    * npm run prettier-writeでフォーマットする
    
    * github actionsでnpm run testを実行する

--- run-with-heroku-env.js
+++ run-with-heroku-env.js
@@ -1,62 +1,61 @@
 #!/usr/bin/env node
 
-const pkg = require('./package.json')
-const {execSync, spawn} = require('child_process')
-const parseArgv = require('./parse-argv')
+const pkg = require('./package.json');
+const { execSync, spawn } = require('child_process');
+const parseArgv = require('./parse-argv');
 
-function getHerokuEnvs ({envs, herokuOptions} = {}) {
-  const option =
-        Object.keys(herokuOptions)
-        .map(key => `--${key} ${herokuOptions[key]}`)
-        .join(' ')
+function getHerokuEnvs({ envs, herokuOptions } = {}) {
+  const option = Object.keys(herokuOptions)
+    .map(key => `--${key} ${herokuOptions[key]}`)
+    .join(' ');
 
-  const cmd = `heroku config ${option}`
-  console.error(`== getting ENV ${envs}: ${cmd}`)
+  const cmd = `heroku config ${option}`;
+  console.error(`== getting ENV ${envs}: ${cmd}`);
 
-  const herokuEnvs = {}
+  const herokuEnvs = {};
   for (let line of execSync(cmd).toString().split(/\n/)) {
-    const [, key, value] = line.match(/^([A-Z\d_]+):\s+(.+)$/) || []
+    const [, key, value] = line.match(/^([A-Z\d_]+):\s+(.+)$/) || [];
     if (key && value && envs.includes(key)) {
-      herokuEnvs[key] = value
+      herokuEnvs[key] = value;
     }
   }
-  return herokuEnvs
+  return herokuEnvs;
 }
 
-function exec ({herokuEnvs, command}) {
-  console.error(`== run: ${command.join(' ')}`)
+function exec({ herokuEnvs, command }) {
+  console.error(`== run: ${command.join(' ')}`);
   const _spawn = spawn(command[0], command.slice(1), {
     stdio: ['pipe', process.stdout, process.stderr],
     env: Object.assign({}, process.env, herokuEnvs)
-  })
-  _spawn.on('close', process.exit)
-  _spawn.on('error', console.error)
+  });
+  _spawn.on('close', process.exit);
+  _spawn.on('error', console.error);
 }
 
-function help () {
+function help() {
   return `
 run-with-heroku-env v${pkg.version}
 Usage:
   % run-with-heroku-env [ENV1 ENV2 ENV3] [heroku options] [execute-command]
   % run-with-heroku-env MONGOLAB_URI --app HEROKU_APP_NAME node scripts/count.js
-`
+`;
 }
 
-function run () {
-  const {envs, herokuOptions, command} = parseArgv(process.argv.slice(2))
+function run() {
+  const { envs, herokuOptions, command } = parseArgv(process.argv.slice(2));
   if (envs.length < 1) {
-    console.error(`Argument Error: ENV is missing`)
-    console.error(help())
-    process.exit(1)
+    console.error(`Argument Error: ENV is missing`);
+    console.error(help());
+    process.exit(1);
   }
-  const herokuEnvs = getHerokuEnvs({envs, herokuOptions})
+  const herokuEnvs = getHerokuEnvs({ envs, herokuOptions });
   for (let key of envs) {
     if (!herokuEnvs[key]) {
-      console.error(`${key} is not found at Heroku ENV`)
-      process.exit(1)
+      console.error(`${key} is not found at Heroku ENV`);
+      process.exit(1);
     }
   }
-  exec({herokuEnvs, command})
+  exec({ herokuEnvs, command });
 }
 
-run()
+run();
